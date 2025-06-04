1a8aaaa2ce79e1188d1e25f91bcdc8d193356338
Kochi23 <167883112+Kochi23@users.noreply.github.com>
2025-06-04 21:54:25 +0900

    パスワードを設定できるように

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ let starttime;
 let maxtime = 180-1;//int
 let interval = null;
 
-const size = 4;
+let size = 0;
+
+let setpassmode = 1;
+let temppass = "";
+let pass = "";
 
 document.getElementById("passbox").addEventListener("keydown", function(e){
     if(e.key === "Enter"){
@@ -10,10 +14,11 @@ document.getElementById("passbox").addEventListener("keydown", function(e){
     }
 });
 
-RefuseBoxSet();
+inputStart();
 
 function RefuseBoxSet(){
     const area = document.getElementById("RefuseBoxArea");
+    area.innerHTML = "";
 
     for(let i=0; i<size; i++){
         area.innerHTML += `<div class='RefuseBox' id='RefuseBox${i}'></div>`;
@@ -31,6 +36,7 @@ function CheckPass(){
 }
 
 function timerStart() {
+    if(setpassmode) return;
     starttime = new Date();
     interval = setInterval(update,70);
     inputStart();
@@ -144,7 +150,7 @@ const keylist  = {
     "FA": "ふぁ", "FI": "ふぃ", "FE": "ふぇ", "FO": "ふぉ",
     "VA": "ゔぁ", "VI": "ゔぃ", "VU": "ゔ", "VE": "ゔぇ", "VO": "ゔぉ",
     "TSA": "つぁ", "TSI": "つぃ", "TSE": "つぇ", "TSO": "つぉ",
-    "CYA": "ちゃ", "CYU": "ちゅ", "CYO": "ちょ",
+    "CYA": "ちゃ", "CYU": "ちゅ", "CYO": "ちょ", "ー":"ー"
 };
 
 function inputStart(){
@@ -159,28 +165,65 @@ function inputEnd(){
 function inputProcessing(e){
     const k = e.key.toUpperCase();
     if(k === "BACKSPACE" && box_counting > 0){
-        if(key_counting.length !== 0){
+        if(setpassmode){
+            ResetInputing();
+        } else {
+            if(key_counting.length !== 0){
             key_counting = key_counting.slice(0,-1);
             SetInput(key_counting,false);
-        } else {
-            deleteInput();
+            } else {
+                deleteInput();
+            }
         }
+        
         return;
     }
     if(box_counting >= size){
         if (k === "ENTER"){
-            let str = "";
-            for(let i=0; i<size; i++){
-                str += document.getElementById(`RefuseBox${i}`).textContent;
-            }
-            if(CanRefuse(str)){
+            if(setpassmode){console.log(temppass)
+                if(pass == ""){
+                    inputEnd();
+                    document.getElementById("RefuseAreaText").textContent = `パスワード:${temppass}`;
+                    document.getElementById("IsSetPassBox1").textContent = "*";
+                    const reservationPass = temppass;
+
+                    setTimeout(() => {
+                        inputStart();
+                        document.getElementById("RefuseAreaText").textContent = "パスワードを確認";
+                        pass = reservationPass;
+                    }, 1000);
+                } else {
+                    if(pass == temppass){
+                        document.getElementById("RefuseAreaText").textContent = "パスワード設定完了";
+                        size = pass.length;
+                        inputEnd();
+                        setTimeout(() => {
+                            RefuseBoxSet();
+                            setpassmode = 0;
+                            document.getElementById("RefuseAreaText").textContent = "爆弾解除コード";
+                        }, 1000);
+                    } else {
+                        ResetInputing();
+                    }
+                }
+
+                temppass = "";
+                key_counting = "";
+            } else {
+                let str = "";
+                for(let i=0; i<size; i++){
+                    str += document.getElementById(`RefuseBox${i}`).textContent;
+                }
+                if(CanRefuse(str)){
                 //クリア
                 timerEnd();
-            } else {
-                ResetInputing();
+                } else {
+                    ResetInputing();
+                }
             }
+            return;
         }
-        return;
+        if(box_counting >= size && !setpassmode) return;
     }
     key_counting += k;
 
@@ -218,7 +261,7 @@ function inputProcessing(e){
 }
 
 function CanRefuse(str){
-    return str === "ばななあ";
+    return str === pass;
 }
 
 function check_key(str){
@@ -226,23 +269,30 @@ function check_key(str){
 }
 
 function SetInput(str,bool){
-    if(box_counting >= size){
-        ResetInputing();
-        return;
-    }
-
-    if(bool===true){
-        let i;
-        for(i=0; i<str.length; i++){
-            if(box_counting+i >= size){
-                ResetInputing();
-                return;
-            }
-            document.getElementById(`RefuseBox${box_counting+i}`).textContent = str[i];
+    if(setpassmode){
+        if(bool){
+            temppass += str;
         }
-        box_counting += 1+(i-1);
+        document.getElementById(`IsSetPassBox${pass == "" ? 1:2}`).textContent = "-";
     } else {
-        document.getElementById(`RefuseBox${box_counting}`).textContent = str;
+        if(box_counting >= size){
+            ResetInputing();
+            return;
+        }
+
+        if(bool===true){
+            let i;
+            for(i=0; i<str.length; i++){
+                if(box_counting+i >= size){
+                    ResetInputing();
+                    return;
+                }
+                document.getElementById(`RefuseBox${box_counting+i}`).textContent = str[i];
+            }
+            box_counting += 1+(i-1);
+        } else {
+            document.getElementById(`RefuseBox${box_counting}`).textContent = str;
+        }
     }
 }
 
@@ -253,9 +303,15 @@ function deleteInput(){
 
 //入力削除
 function ResetInputing(){
-    for(let i=0; i<size; i++){
-        document.getElementById(`RefuseBox${i}`).textContent = "";
+    if(setpassmode){
+        temppass = "";
+        key_counting = "";
+        document.getElementById(`IsSetPassBox${pass == "" ? 1:2}`).textContent = "";
+    } else{
+        for(let i=0; i<size; i++){
+            document.getElementById(`RefuseBox${i}`).textContent = "";
+        }
+        key_counting = "";
+        box_counting = 0;
     }
-    key_counting = "";
-    box_counting = 0;
 }
